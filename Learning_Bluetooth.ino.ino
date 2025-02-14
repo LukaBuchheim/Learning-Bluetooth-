3439d6e6b1153225f73663a7cebfd4d203070fe0
Luka Buchheim <lukabuchheim@lukas-mbp-2.milton.edu>
2025-02-14 13:53:50 -0500

    finsihed code

--- Learning_Bluetooth.ino.ino
+++ Learning_Bluetooth.ino.ino
@@ -5,12 +5,28 @@ int const TX_PIN = 3; //sends signal
 SoftwareSerial tooth(TX_PIN, RX_PIN); //make a bluetooth object 
 //set tx and rx pins 
 //tx goes first, then rx 
+char davis; 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600); 
+  tooth.begin(9600); 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  if( tooth.available() > 0){
+    
+    davis =tooth.read();
+    tooth.print("reading new input: ");
+    tooth.println(davis);
+  }
+  
+  if(davis == 'd' ){
+Serial.println("davis has value!"); 
+delay(100);
+
+  }
 
 }
